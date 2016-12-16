ce1416d1157008a7928dbe23c857bf480383f5c2
xiaobailong24 <xiaobailong24@gmail.com>
2016-12-16 09:01:59 +0800

    Add Database SQL

--- src/com/tjkcht/databaseexport/Start.java
+++ src/com/tjkcht/databaseexport/Start.java
@@ -11,15 +11,4 @@ public class Start {
         UiInit.start();
     }
 
-/*
-    public static List<String> findJar(String txtPath) throws IOException {
-        List<String> list = new ArrayList<>();
-        Files.readAllLines(Paths.get(txtPath), Charset.forName("GBK")).forEach(s -> {
-            if (s.contains("DatabaseExport_jar") && !list.contains(s.split("DatabaseExport_jar")[1])) {
-                list.add(s.split("DatabaseExport_jar")[1]);
-            }
-        });
-        return list;
-    }*/
-
 }
